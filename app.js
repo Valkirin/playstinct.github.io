55afeb18be0c4e72602da3ea261c2f3eaa6f2274
Viktor <valkirinn@gmail.com>
2022-09-07 13:15:43 +0300

    add files

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
 let btn3 = document.getElementById('btn3');
 let btn4 = document.getElementById('btn4');
-let btn5 = document.getElementById('btn5');
-let btn6 = document.getElementById('btn6');
 
 btn1.addEventListener('click', function () {
   if (tg.MainButton.isVisible) {
@@ -50,26 +48,6 @@ btn4.addEventListener('click', function () {
   } else {
     tg.MainButton.setText('Сделать ставку 4!');
     item = '4';
-    tg.MainButton.show();
-  }
-});
-
-btn5.addEventListener('click', function () {
-  if (tg.MainButton.isVisible) {
-    tg.MainButton.hide();
-  } else {
-    tg.MainButton.setText('Сделать ставку 5!');
-    item = '5';
-    tg.MainButton.show();
-  }
-});
-
-btn6.addEventListener('click', function () {
-  if (tg.MainButton.isVisible) {
-    tg.MainButton.hide();
-  } else {
-    tg.MainButton.setText('Сделать ставку 6!');
-    item = '6';
     tg.MainButton.show();
   }
 });
